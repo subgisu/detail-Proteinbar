aca84c18a0f575e4a8bf121008b41cfcb9e6acff
subgisu <annjin4@gmail.com>
2026-03-20 11:06:50 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/-Protein_detail/',
+    base: '/detail-Proteinbar/',
   plugins: [react(), tailwindcss()],
 });
